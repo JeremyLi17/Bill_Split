f3c5d90a36f809683b58e38343b1e0be2d816831
JeremyLi17 <HaotianLi0505@hotmail.com>
2023-01-24 14:47:53 -0500

    server

---
+++ server/src/main/java/com/easypay/EasypayApplication.java
@@ -0,0 +1,13 @@
+package com.easypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasypayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasypayApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/easypay/EasypayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasypayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
